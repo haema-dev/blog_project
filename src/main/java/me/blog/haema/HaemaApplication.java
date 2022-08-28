aa705846bf68c0a65fe3529c67663a73d1d3789c
SUA <haemahmtea@gmail.com>
2022-08-28 15:15:34 +0900

    init

---
+++ src/main/java/me/blog/haema/HaemaApplication.java
@@ -0,0 +1,13 @@
+package me.blog.haema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/blog/haema/HaemaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.blog.haema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
